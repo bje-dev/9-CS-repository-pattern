81c275c6e29b9996c5d007db352d121b73985b85
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2022-12-11 15:06:44 -0300

    Initial commit

---
+++ DOM/Address.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOM
+{
+    public class Address
+    {
+
+        public Guid IdAddress { get; set; }
+
+        private string _Street;
+       
+
+        public string Street 
+        {
+
+            get
+            {
+                return _Street;
+            }
+
+            set
+            {
+                _Street = value;
+            }
+        }
+
+        private int Number { get; set; }
+
+
+        private string City { get; set; }
+
+        
+        public Address()
+        {
+
+        }
+
+        public Address(Guid IdAddress, string Street, int Number, string City)
+        {
+            this.IdAddress = IdAddress;
+            this.Street = Street;
+            this.Number = Number;
+            this.City = City;
+        }
+
+    }
+}
---
+++ DOM/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOM
+{
+    class Customer
+    {
+        public Guid IdCustomer { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateBirth { get; set; }
+        public string Doc { get; set; }
+        public Address Address { get; set; }
+
+        public Customer()
+        {
+
+        }
+
+        public Customer( string FirstName, string LastName, DateTime DateBirth, string Doc, Address Address, Guid? IdCustomer)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.DateBirth = DateBirth;
+            this.Doc = Doc;
+            this.Address = Address;
+
+        }
+    }
+}
